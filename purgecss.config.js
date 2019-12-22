3f27233153980538f4972f040b92eb1cc9282a54
suchaimi <suchaimi.afif@gmail.com>
2019-12-23 00:47:53 +0700

    Fixing doc page issues

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,52 +1,34 @@
-// class TailwindExtractor {
-//   static extract(content) {
-//     return content.match(/[A-z0-9-:\\/]+/g)
-//   }
-// }
+class TailwindExtractor {
+  static extract(content) {
+    return content.match(/[A-z0-9-:\\/]+/g)
+  }
+}
 
-// module.exports = {
-//   content: [
-//     './src/**/*.vue',
-//     './src/**/*.js',
-//     './src/**/*.jsx',
-//     './src/**/*.html',
-//     './src/**/*.pug',
-//     './src/**/*.md',
-//     './docs/**/*.md',
-//     './blog/**/*.md',
-//   ],
-//   whitelist: [
-//     'body',
-//     'html',
-//     'img',
-//     'a',
-//     'g-image',
-//     'g-image--lazy',
-//     'g-image--loaded',
-//     'active',
-//   ],
-//   extractors: [
-//     {
-//       extractor: TailwindExtractor,
-//       extensions: ['vue', 'js', 'jsx', 'md', 'html', 'pug'],
-//     },
-//   ],
-// }
-
-new purgeCss({
-            paths: glob.sync([
-                path.join(__dirname, 'resources/views/**/*.blade.php'),
-                path.join(__dirname, 'resources/assets/js/**/*.vue'),
-                path.join(__dirname, 'resources/assets/vendor/**/*.vue')
-            ]),
-            extractors: [
-                {
-                    extractor: class {
-                        static extract(content) {
-                            return content.match(/[A-z0-9-:\/]+/g) || []
-                        }
-                    },
-                    extensions: ['html', 'js', 'php', 'vue']
-                }
-            ]
-        })
+module.exports = {
+  content: [
+    './src/**/*.vue',
+    './src/**/*.js',
+    './src/**/*.jsx',
+    './src/**/*.html',
+    './src/**/*.pug',
+    './src/**/*.md',
+    './docs/**/*.md',
+    './blog/**/*.md',
+  ],
+  whitelist: [
+    'body',
+    'html',
+    'img',
+    'a',
+    'g-image',
+    'g-image--lazy',
+    'g-image--loaded',
+    'active',
+  ],
+  extractors: [
+    {
+      extractor: TailwindExtractor,
+      extensions: ['vue', 'js', 'jsx', 'md', 'html', 'pug'],
+    },
+  ],
+}
